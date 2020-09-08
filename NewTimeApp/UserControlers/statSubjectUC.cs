0e1bd87276a8e84fdcd51c6bb4d12f0dc786e82c
chamo000 <chamodyawanaguru96@gmail.com>
2020-09-08 16:02:21 +0530

    IT18203554

---
+++ NewTimeApp/UserControlers/statSubjectUC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewTimeApp.UserControlers
+{
+    public partial class statSubjectUC : UserControl
+    {
+        public statSubjectUC()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
