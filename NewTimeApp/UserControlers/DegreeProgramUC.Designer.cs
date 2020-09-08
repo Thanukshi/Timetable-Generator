6085210efe0982454215c0076169d906685dda30
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-09-08 17:38:30 +0530

    IT18202632

--- NewTimeApp/UserControlers/DegreeProgramUC.Designer.cs
+++ NewTimeApp/UserControlers/DegreeProgramUC.Designer.cs
@@ -30,13 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DegreeProgramUC));
             this.degreePanel = new System.Windows.Forms.Panel();
-            this.backBtnD = new System.Windows.Forms.Button();
-            this.saveDe = new ePOSOne.btnProduct.Button_WOC();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.programNameD = new System.Windows.Forms.TextBox();
             this.preferNameD = new System.Windows.Forms.TextBox();
+            this.programNameD = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backBtnD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveDe = new ePOSOne.btnProduct.Button_WOC();
             this.degreePanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,6 +56,50 @@
             this.degreePanel.Size = new System.Drawing.Size(1000, 790);
             this.degreePanel.TabIndex = 0;
             // 
+            // preferNameD
+            // 
+            this.preferNameD.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.preferNameD.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.preferNameD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.preferNameD.Location = new System.Drawing.Point(453, 332);
+            this.preferNameD.Multiline = true;
+            this.preferNameD.Name = "preferNameD";
+            this.preferNameD.Size = new System.Drawing.Size(470, 45);
+            this.preferNameD.TabIndex = 34;
+            // 
+            // programNameD
+            // 
+            this.programNameD.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.programNameD.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.programNameD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.programNameD.Location = new System.Drawing.Point(453, 220);
+            this.programNameD.Multiline = true;
+            this.programNameD.Name = "programNameD";
+            this.programNameD.Size = new System.Drawing.Size(470, 45);
+            this.programNameD.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.label3.Location = new System.Drawing.Point(115, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 38);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Prefered Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.label2.Location = new System.Drawing.Point(115, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 38);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Program Name";
+            // 
             // backBtnD
             // 
             this.backBtnD.FlatAppearance.BorderSize = 0;
@@ -67,6 +111,17 @@
             this.backBtnD.TabIndex = 30;
             this.backBtnD.UseVisualStyleBackColor = true;
             this.backBtnD.Click += new System.EventHandler(this.backBtnD_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.label1.Location = new System.Drawing.Point(115, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 41);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Degree Programs";
             // 
             // saveDe
             // 
@@ -80,7 +135,7 @@
             this.saveDe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.saveDe.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.saveDe.ForeColor = System.Drawing.Color.Black;
-            this.saveDe.Location = new System.Drawing.Point(507, 511);
+            this.saveDe.Location = new System.Drawing.Point(506, 431);
             this.saveDe.Name = "saveDe";
             this.saveDe.OnHoverBorderColor = System.Drawing.Color.White;
             this.saveDe.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
@@ -90,61 +145,6 @@
             this.saveDe.Text = "Save";
             this.saveDe.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.saveDe.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.label1.Location = new System.Drawing.Point(115, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(265, 41);
-            this.label1.TabIndex = 24;
-            this.label1.Text = "Degree Programs";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.label2.Location = new System.Drawing.Point(115, 215);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(206, 38);
-            this.label2.TabIndex = 31;
-            this.label2.Text = "Program Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.label3.Location = new System.Drawing.Point(115, 324);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(217, 38);
-            this.label3.TabIndex = 32;
-            this.label3.Text = "Prefered Name";
-            // 
-            // programNameD
-            // 
-            this.programNameD.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.programNameD.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.programNameD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.programNameD.Location = new System.Drawing.Point(453, 220);
-            this.programNameD.Multiline = true;
-            this.programNameD.Name = "programNameD";
-            this.programNameD.Size = new System.Drawing.Size(470, 45);
-            this.programNameD.TabIndex = 33;
-            // 
-            // preferNameD
-            // 
-            this.preferNameD.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.preferNameD.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.preferNameD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.preferNameD.Location = new System.Drawing.Point(453, 332);
-            this.preferNameD.Multiline = true;
-            this.preferNameD.Name = "preferNameD";
-            this.preferNameD.Size = new System.Drawing.Size(470, 45);
-            this.preferNameD.TabIndex = 34;
             // 
             // DegreeProgramUC
             // 
