021f449232d71cce85b863a0d46c13e3d7822e84
chamo000 <chamodyawanaguru96@gmail.com>
2020-09-10 02:07:55 +0530

    IT18203554

---
+++ NewTimeApp/UserControlers/viewroomsUC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewTimeApp.UserControlers
+{
+    public partial class viewroomsUC : UserControl
+    {
+        public viewroomsUC()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
