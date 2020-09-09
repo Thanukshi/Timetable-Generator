8fbe0e9c67d633e98fdaa78b718110adee95f61b
chamo000 <chamodyawanaguru96@gmail.com>
2020-09-10 01:51:40 +0530

    IT18203554

--- NewTimeApp/UserControlers/statSubjectUC.Designer.cs
+++ NewTimeApp/UserControlers/statSubjectUC.Designer.cs
@@ -30,10 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(statSubjectUC));
             this.statSubjectpanel = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.backBtn = new System.Windows.Forms.Button();
-            this.subyearBtn = new ePOSOne.btnProduct.Button_WOC();
             this.subgraphpanel = new System.Windows.Forms.Panel();
+            this.subyearBtn = new ePOSOne.btnProduct.Button_WOC();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.statSubjectpanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,29 +49,13 @@
             this.statSubjectpanel.Size = new System.Drawing.Size(772, 720);
             this.statSubjectpanel.TabIndex = 0;
             // 
-            // label1
+            // subgraphpanel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
-            this.label1.Location = new System.Drawing.Point(76, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(369, 35);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Statistics Related to Subjects";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // backBtn
-            // 
-            this.backBtn.FlatAppearance.BorderSize = 0;
-            this.backBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.backBtn.Image = ((System.Drawing.Image)(resources.GetObject("backBtn.Image")));
-            this.backBtn.Location = new System.Drawing.Point(21, 14);
-            this.backBtn.Margin = new System.Windows.Forms.Padding(2);
-            this.backBtn.Name = "backBtn";
-            this.backBtn.Size = new System.Drawing.Size(50, 46);
-            this.backBtn.TabIndex = 28;
-            this.backBtn.UseVisualStyleBackColor = true;
+            this.subgraphpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.subgraphpanel.Location = new System.Drawing.Point(21, 161);
+            this.subgraphpanel.Name = "subgraphpanel";
+            this.subgraphpanel.Size = new System.Drawing.Size(733, 540);
+            this.subgraphpanel.TabIndex = 30;
             // 
             // subyearBtn
             // 
@@ -97,13 +81,30 @@
             this.subyearBtn.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.subyearBtn.UseVisualStyleBackColor = false;
             // 
-            // subgraphpanel
+            // backBtn
             // 
-            this.subgraphpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.subgraphpanel.Location = new System.Drawing.Point(21, 161);
-            this.subgraphpanel.Name = "subgraphpanel";
-            this.subgraphpanel.Size = new System.Drawing.Size(733, 540);
-            this.subgraphpanel.TabIndex = 30;
+            this.backBtn.FlatAppearance.BorderSize = 0;
+            this.backBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backBtn.Image = ((System.Drawing.Image)(resources.GetObject("backBtn.Image")));
+            this.backBtn.Location = new System.Drawing.Point(21, 14);
+            this.backBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(50, 46);
+            this.backBtn.TabIndex = 28;
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(4)))), ((int)(((byte)(45)))));
+            this.label1.Location = new System.Drawing.Point(76, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Statistics Related to Subjects";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // statSubjectUC
             // 
