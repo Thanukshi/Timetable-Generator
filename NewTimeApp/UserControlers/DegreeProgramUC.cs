d9420ca64be23b64289cbb77c942f4345beab014
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-09-10 22:11:22 +0530

    IT18202632

--- NewTimeApp/UserControlers/DegreeProgramUC.cs
+++ NewTimeApp/UserControlers/DegreeProgramUC.cs
@@ -8,14 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using NewTimeApp.Helpers;
+using System.Data.SqlClient;
 
 namespace NewTimeApp.UserControlers
 {
     public partial class DegreeProgramUC : UserControl
     {
+        string con = "Data Source=DESKTOP-PHJQSJE;Initial Catalog=NewTimeApp;Integrated Security=True";
+        SqlConnection sqlCon;
+        SqlCommand sqlCom;
+        string degreeID = "";
+
         public DegreeProgramUC()
         {
             InitializeComponent();
+            sqlCon = new SqlConnection(con);
+            sqlCon.Open();
         }
 
         private void backBtnD_Click(object sender, EventArgs e)
