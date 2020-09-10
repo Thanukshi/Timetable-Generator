f68c66aa75e1a5a8f1ef5db9a5ea5ca33561d442
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-09-10 14:04:48 +0530

    IT18202632

---
+++ NewTimeApp/Helpers/AcademicDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTimeApp.Helpers
+{
+    class AcademicDetails
+    {
+        public string academicYear { get; set; }
+
+        public string academicSemester { get; set; }
+    }
+}
