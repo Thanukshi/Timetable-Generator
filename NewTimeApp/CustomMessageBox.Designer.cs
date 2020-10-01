9090c4f5ac304e9bc02b92049ba06766a215fad7
chamo000 <chamodyawanaguru96@gmail.com>
2020-10-01 20:45:02 +0530

    IT18203554

--- NewTimeApp/CustomMessageBox.Designer.cs
+++ NewTimeApp/CustomMessageBox.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CustomMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 214);
+            this.Name = "CustomMessageBox";
             this.Text = "CustomMessageBox";
+            this.Load += new System.EventHandler(this.CustomMessageBox_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
