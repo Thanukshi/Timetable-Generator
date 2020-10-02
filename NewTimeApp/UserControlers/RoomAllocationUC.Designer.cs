5bcf9f33e9d105644309d82fb216e4160163fd3d
chamo000 <chamodyawanaguru96@gmail.com>
2020-10-02 20:07:54 +0530

    IT18203554

--- NewTimeApp/UserControlers/RoomAllocationUC.Designer.cs
+++ NewTimeApp/UserControlers/RoomAllocationUC.Designer.cs
@@ -28,14 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomAllocationUC));
             this.RoomAllocationPanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.circularButton1 = new NewTimeApp.CircularButton();
+            this.circularButton2 = new NewTimeApp.CircularButton();
+            this.circularButton3 = new NewTimeApp.CircularButton();
+            this.circularButton4 = new NewTimeApp.CircularButton();
+            this.circularButton5 = new NewTimeApp.CircularButton();
+            this.circularButton6 = new NewTimeApp.CircularButton();
             this.RoomAllocationPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // RoomAllocationPanel
             // 
+            this.RoomAllocationPanel.Controls.Add(this.circularButton6);
+            this.RoomAllocationPanel.Controls.Add(this.circularButton5);
+            this.RoomAllocationPanel.Controls.Add(this.circularButton4);
+            this.RoomAllocationPanel.Controls.Add(this.circularButton3);
+            this.RoomAllocationPanel.Controls.Add(this.circularButton2);
+            this.RoomAllocationPanel.Controls.Add(this.circularButton1);
             this.RoomAllocationPanel.Controls.Add(this.pictureBox1);
             this.RoomAllocationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.RoomAllocationPanel.Location = new System.Drawing.Point(0, 0);
@@ -53,6 +66,114 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // circularButton1
+            // 
+            this.circularButton1.BackColor = System.Drawing.Color.Plum;
+            this.circularButton1.FlatAppearance.BorderSize = 0;
+            this.circularButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton1.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton1.ForeColor = System.Drawing.Color.White;
+            this.circularButton1.Image = ((System.Drawing.Image)(resources.GetObject("circularButton1.Image")));
+            this.circularButton1.Location = new System.Drawing.Point(66, 510);
+            this.circularButton1.Name = "circularButton1";
+            this.circularButton1.Size = new System.Drawing.Size(170, 170);
+            this.circularButton1.TabIndex = 37;
+            this.circularButton1.Text = "Room Allocations";
+            this.circularButton1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton1.UseVisualStyleBackColor = false;
+            // 
+            // circularButton2
+            // 
+            this.circularButton2.BackColor = System.Drawing.Color.Plum;
+            this.circularButton2.FlatAppearance.BorderSize = 0;
+            this.circularButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton2.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton2.ForeColor = System.Drawing.Color.White;
+            this.circularButton2.Image = ((System.Drawing.Image)(resources.GetObject("circularButton2.Image")));
+            this.circularButton2.Location = new System.Drawing.Point(518, 499);
+            this.circularButton2.Name = "circularButton2";
+            this.circularButton2.Size = new System.Drawing.Size(170, 170);
+            this.circularButton2.TabIndex = 38;
+            this.circularButton2.Text = "Room Allocations";
+            this.circularButton2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton2.UseVisualStyleBackColor = false;
+            // 
+            // circularButton3
+            // 
+            this.circularButton3.BackColor = System.Drawing.Color.Plum;
+            this.circularButton3.FlatAppearance.BorderSize = 0;
+            this.circularButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton3.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton3.ForeColor = System.Drawing.Color.White;
+            this.circularButton3.Image = ((System.Drawing.Image)(resources.GetObject("circularButton3.Image")));
+            this.circularButton3.Location = new System.Drawing.Point(66, 278);
+            this.circularButton3.Name = "circularButton3";
+            this.circularButton3.Size = new System.Drawing.Size(170, 170);
+            this.circularButton3.TabIndex = 39;
+            this.circularButton3.Text = "Room Allocations";
+            this.circularButton3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton3.UseVisualStyleBackColor = false;
+            // 
+            // circularButton4
+            // 
+            this.circularButton4.BackColor = System.Drawing.Color.Plum;
+            this.circularButton4.FlatAppearance.BorderSize = 0;
+            this.circularButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton4.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton4.ForeColor = System.Drawing.Color.White;
+            this.circularButton4.Image = ((System.Drawing.Image)(resources.GetObject("circularButton4.Image")));
+            this.circularButton4.Location = new System.Drawing.Point(531, 278);
+            this.circularButton4.Name = "circularButton4";
+            this.circularButton4.Size = new System.Drawing.Size(170, 170);
+            this.circularButton4.TabIndex = 40;
+            this.circularButton4.Text = "Room Allocations";
+            this.circularButton4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton4.UseVisualStyleBackColor = false;
+            // 
+            // circularButton5
+            // 
+            this.circularButton5.BackColor = System.Drawing.Color.Plum;
+            this.circularButton5.FlatAppearance.BorderSize = 0;
+            this.circularButton5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton5.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton5.ForeColor = System.Drawing.Color.White;
+            this.circularButton5.Image = ((System.Drawing.Image)(resources.GetObject("circularButton5.Image")));
+            this.circularButton5.Location = new System.Drawing.Point(43, 56);
+            this.circularButton5.Name = "circularButton5";
+            this.circularButton5.Size = new System.Drawing.Size(170, 170);
+            this.circularButton5.TabIndex = 41;
+            this.circularButton5.Text = "Room Allocations";
+            this.circularButton5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton5.UseVisualStyleBackColor = false;
+            // 
+            // circularButton6
+            // 
+            this.circularButton6.BackColor = System.Drawing.Color.Plum;
+            this.circularButton6.FlatAppearance.BorderSize = 0;
+            this.circularButton6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.circularButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.circularButton6.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularButton6.ForeColor = System.Drawing.Color.White;
+            this.circularButton6.Image = ((System.Drawing.Image)(resources.GetObject("circularButton6.Image")));
+            this.circularButton6.Location = new System.Drawing.Point(540, 56);
+            this.circularButton6.Name = "circularButton6";
+            this.circularButton6.Size = new System.Drawing.Size(170, 170);
+            this.circularButton6.TabIndex = 42;
+            this.circularButton6.Text = "Room Allocations";
+            this.circularButton6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.circularButton6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.circularButton6.UseVisualStyleBackColor = false;
             // 
             // RoomAllocationUC
             // 
@@ -72,5 +193,11 @@
 
         private System.Windows.Forms.Panel RoomAllocationPanel;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private CircularButton circularButton6;
+        private CircularButton circularButton5;
+        private CircularButton circularButton4;
+        private CircularButton circularButton3;
+        private CircularButton circularButton2;
+        private CircularButton circularButton1;
     }
 }
